50c42659f6430811276b242063623427ae6b8459
Naveen Kumar PS <naveenkumar04122004@gmail.com>
2026-03-04 22:51:09 +0530

    Initial commit: first version of the project

---
+++ script.js
@@ -0,0 +1,156 @@
+let gameSave  =  JSON.parse(localStorage.getItem('gameSave')) ||
+{
+  Wins:0,
+  Lose:0,
+  Tie:0,
+  lastResult : '',
+  lastPlayerMove : '',
+  lastComputerMove : ''
+};
+
+updateScore();
+
+let isAutoPlay = false;
+let intervalId;
+
+function autoPlay() {
+
+  if(!isAutoPlay) {
+    intervalId = setInterval(function() {
+      const playerMove = computerMove();
+      playGame(playerMove);
+      },1000);
+    isAutoPlay = true;
+  }
+  else {
+    clearInterval(intervalId)
+    isAutoPlay = false;
+  }
+}
+
+
+function playGame(playerMove) {
+  const randomMove = computerMove();
+  if(playerMove === 'Reset') {
+    gameSave.Wins = 0;
+    gameSave.Lose = 0;
+    gameSave.Tie = 0;
+    gameSave.lastResult = '';
+    gameSave.lastPlayerMove = '';
+    gameSave.lastComputerMove = '';
+
+    localStorage.removeItem('gameSave');
+    updateScore();
+    resultMoves('','','');
+    return;
+  }
+
+  const result = playerMoveResult(playerMove,randomMove)
+  
+
+  if(result === 'You Win') {
+    gameSave.Wins++;
+  }
+  else if(result ==='Tie') {
+    gameSave.Tie++;
+  }
+  else if(result === 'You Lose') {
+    gameSave.Lose++;
+  }
+
+  gameSave.lastResult = result;
+  gameSave.lastPlayerMove = playerMove;
+  gameSave.lastComputerMove = randomMove;
+
+  localStorage.setItem('gameSave',JSON.stringify(gameSave));
+
+  resultMoves(result,playerMove,randomMove);
+  updateScore();
+}
+
+function computerMove() {
+  let computerMove = '';
+  const randomNumber = Math.random();
+
+  if(randomNumber>= 0 && randomNumber < (1/3)) {
+    computerMove = 'Rock';
+  }
+  else if(randomNumber < (2/3)) {
+    computerMove = 'Paper';
+  }
+  else {
+    computerMove = 'Scissors';
+  }
+  return computerMove;
+}
+
+function playerMoveResult(playerMove,randomMove) {
+  let result = '';
+  if(playerMove === 'Rock') {
+    if(randomMove === 'Rock') {
+      result = 'Tie';
+    }
+    else if(randomMove === 'Paper') {
+      result = 'You Lose';
+    }
+    else if(randomMove === 'Scissors') {
+      result = 'You Win';
+    }
+  }
+
+  else if(playerMove === 'Paper') {
+    if(randomMove === 'Rock') {
+        result = 'You Win';
+    }
+    else if(randomMove === 'Paper') {
+      result = 'Tie';
+    }
+    else if(randomMove === 'Scissors') {
+      result = 'You Lose';
+    }
+
+  }
+
+  else if(playerMove === 'Scissors') {
+    if(randomMove === 'Rock') {
+      result = 'You Lose';
+    }
+    else if(randomMove === 'Paper') {
+      result = 'You Win';
+    }
+    else if(randomMove === 'Scissors') {
+      result = 'Tie';
+    }
+  }
+
+  return result;
+}
+
+function resultMoves(result,playerMove,randomMove) {
+  if(result === '' && playerMove === '' && randomMove ==='') {
+    document.querySelector('.js-result')
+      .innerHTML = 'None';
+
+    document.querySelector('.js-moves')
+      .innerHTML = `Reseted`;
+    return;
+  }
+  document.querySelector('.js-result')
+    .innerHTML = result;
+  
+  document.querySelector('.js-moves')
+    .innerHTML = `You
+    <img src="images/${playerMove}-emoji.png" class="cs-moves">
+    <img src="images/${randomMove}-emoji.png" class="cs-moves">
+    computer`;
+}
+
+function updateScore() {
+  if(gameSave.lastResult) {
+    resultMoves(gameSave.lastResult,
+    gameSave.lastPlayerMove,
+    gameSave.lastComputerMove)
+  }
+  document.querySelector('.js-scores')
+    .innerHTML = `Wins:${gameSave.Wins},Losses:${gameSave.Lose},Ties:${gameSave.Tie}`;
+}
